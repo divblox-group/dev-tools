24de2e96d473b4a70fbe14921f877f49d9da65eb
brett.coetzee@stratusolve.com <brett.coetzee@stratusolve.com>
2024-09-23 12:47:54 +0200

    added echos for missing args

--- git.sh
+++ git.sh
@@ -1,20 +1,29 @@
 if [ -z $1 ]; then
+    echo 'arg1 project folder not defined';
     exit 1
 fi
 if [ -z $2 ]; then
+    echo 'arg2 module folder (sub folder) not defined';
     exit 1
 fi
 if [ -z $3 ]; then
+    echo 'arg3 git branch not defined';
     exit 1
 fi
 if [ -z $4 ]; then
+    echo 'arg4 branch suffix not defined';
     exit 1
 fi
-folder=$1
-branch=$2
-branchSuffix=$3
-repository=$4
-if cd ${folder}; then
+if [ -z $5 ]; then
+    echo 'arg5 git repo not defined';
+    exit 1
+fi
+project=$1
+folder=$2
+branch=$3
+branchSuffix=$4
+repository=$5
+if cd ../${project}/${folder}; then
     git config core.filemode false
     git stash
     git reset --hard
@@ -22,11 +31,12 @@ if cd ${folder}; then
     git remote set-branches origin ${branch} ${branch}*
     git pull origin ${branch}
     git pull origin ${branch}-${branchSuffix}
-    cd ..
+    cd ../dev-tools
 else
+    cd ../${project}
     git clone --branch ${branch} --single-branch ${repository} ./${folder}
     cd ${folder}
     git branch ${branch}-${branchSuffix}
     git checkout ${branch}-${branchSuffix}
-    cd ..
+    cd ../dev-tools
 fi
